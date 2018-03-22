c0411be496750951010ebd4e322ba07038134b20
Tristan Gordon <tgordon@benefitcommunications.com>
2018-03-22 03:46:27 -0500

    remove events.json

--- src/app/ng-events-routing.module.ts
+++ src/app/ng-events-routing.module.ts
@@ -2,9 +2,11 @@ import { RouterModule, Routes } from "@angular/router";
 import { NgModule } from "@angular/core";
 
 import { NgEventsComponent } from "./ng-events/ng-events.component";
+import { NgEventDetailsComponent } from "./ng-event-details/ng-event-details.component";
 
 export const routes: Routes = [
-    { path: "", component: NgEventsComponent }
+    { path: "", component: NgEventsComponent },
+    { path: "event-details/:id", component: NgEventDetailsComponent }
 ];
 
 @NgModule({
